fbca5eaed90bb51a2ac87ec7306055e27877ccad
00014055 <wiut00014055@gmail.com>
2024-03-16 03:02:55 +0500

    Validation added

--- CarRent/Models/Car.cs
+++ CarRent/Models/Car.cs
@@ -1,4 +1,6 @@
 ﻿using CarRent.Data;
+//using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing.Printing;
 
@@ -7,12 +9,20 @@ namespace CarRent.Models
     public class Car
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "filling in this field is required")]
         public string Brand { get; set; }
+
+        [Required(ErrorMessage = "filling in this field is required")]
         public string Model { get; set; }
+
+        [Required(ErrorMessage = "filling in this field is required")]
         public int Year { get; set; }
+
+        [Required(ErrorMessage = "filling in this field is required")]
         public string Color { get; set; }
 
-
+        [Required(ErrorMessage = "filling in this field is required")]
         public int? CustomerId { get; set; }
 
         [ForeignKey("CustomerId")]
--- CarRent/Models/Customers.cs
+++ CarRent/Models/Customers.cs
@@ -1,10 +1,19 @@
-﻿namespace CarRent.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRent.Models
 {
     public class Customers
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "filling in this field is required")]
         public string customer_name { get; set; }
+
+        [Required(ErrorMessage = "filling in this field is required")]
         public string customer_email { get; set;}
+
+        [Required(ErrorMessage = "filling in this field is required")]
         public string customer_phone { get; set;}
     }
 }
